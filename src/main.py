78cf2f03143de2a7e1009339223d7b82b2e86be1
titi-lima <titisau@gmail.com>
2023-04-19 16:12:30 -0300

    fix: button controller

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from utils.buttons import ButtonController
+
+button_controller = ButtonController()
+
+button_array = button_controller.get_button_array()
+
+print(button_array)
--- src/utils/buttons.py
+++ src/utils/buttons.py
@@ -7,13 +7,13 @@ fd = os.open('/dev/mydev', os.O_RDWR)
 
 class ButtonController:
     # Get button binary array
-    def get_button_array():
+    def get_button_array(self):
         ioctl(fd, RD_PBUTTONS)
         red = os.read(fd, 4)  # read 4 bytes and store in red var
-        return int.from_bytes(red, 'little')
+        return "{0:b}".format(int.from_bytes(red, 'little')).zfill(4)
 
     # Get individual button binary value
-    def get_button_value(button):
+    def get_button_value(self, button):
         ioctl(fd, RD_PBUTTONS)
         red = os.read(fd, 4)  # read 4 bytes and store in red var
         print("red 0x%X" % int.from_bytes(red, 'little'))
