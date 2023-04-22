87e414bef899ffd5505a214fc8290788739a2b11
Júlia Dias <julia.dias@citi.org.br>
2023-04-22 08:54:27 -0300

    feat: number converter

--- src/mock/switches.py
+++ src/mock/switches.py
@@ -1,7 +1,15 @@
 class SwitchController:
     # Get switch binary array
     def get_switch_array(self):
-        return "000000000000000000"
+        return "000000001000000110"
+    
+    def get_switch_number_1(self):
+        switch = self.get_switch_array()[0:9]
+        return int(switch, 2)
+    
+    def get_switch_number_2(self):
+        switch = self.get_switch_array()[9:18]
+        return int(switch, 2)
 
     # Get individual switch binary value
     def get_switch_value(self, switch):
