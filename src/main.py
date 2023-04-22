308dacf31572278f15f01ae83c44d09041e4cf5d
Júlia Dias <julia.dias@citi.org.br>
2023-04-22 05:14:57 -0300

    feat: generate multiple random numbers

--- src/main.py
+++ src/main.py
@@ -1,6 +1,21 @@
 import pygame
 import random
 
+# Função para gerar um novo número aleatório
+def gerar_numero_aleatorio():
+    global numero_aleatorio
+    # Define a cor do retângulo a ser desenhado para apagar o número anterior
+    cor_fundo = (0, 0, 0)
+    # Desenha um retângulo preenchido na posição do número anterior
+    pygame.draw.rect(screen, cor_fundo, (posicao_texto[0] - 50, posicao_texto[1] - 50, 100, 100))
+    numero_aleatorio = random.randint(1, 100)
+    # Renderiza o novo número aleatório como um texto
+    texto_numero = fonte.render(str(numero_aleatorio), True, (255, 255, 255))
+    # Desenha o novo número na tela
+    screen.blit(texto_numero, posicao_texto)
+    # Atualiza toda a tela
+    pygame.display.flip()
+
 # Gera o número aleatório
 numero_aleatorio = random.randint(1, 100)
 
@@ -35,6 +50,12 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                # Gerar um novo número aleatório quando o usuário pressiona Enter
+                gerar_numero_aleatorio()
+
+
 
     # Atualiza a tela do jogo
     pygame.display.update()
