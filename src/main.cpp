1759b540a3235eaa2a1f9d7329b763130140ab20
diasjulia <jedl@cin.ufpe.br>
2023-04-22 07:51:45 -0300

    feat: basic game in c

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
-#include <stdio.h>	/* printf */
-#include <stdlib.h>	/* malloc, atoi, rand... */
-#include <string.h>	/* memcpy, strlen... */
-#include <stdint.h>	/* uints types */
-#include <sys/types.h>	/* size_t ,ssize_t, off_t... */
-#include <unistd.h>	/* close() read() write() */
-#include <fcntl.h>	/* open() */
-#include <sys/ioctl.h>	/* ioctl() */
-#include <errno.h>	/* error codes */
+#include "raylib.h"
+#include <stdio.h>
 
-// ioctl commands defined for the pci driver header
-#include "ioctl_cmds.h"
-
-int main(int argc, char** argv)
+int main()
 {
-	printf("hello world\n");
+	// Define as dimensões da janela
+	int screenWidth = 800;
+	int screenHeight = 600;
+
+	// Inicia o raylib e cria a janela
+	InitWindow(screenWidth, screenHeight, "Meu Jogo");
+
+	// Gera um número aleatório entre 1 e 100
+	int numeroAleatorio = GetRandomValue(1, 100);
+
+	// Loop principal do jogo
+	while (!WindowShouldClose())
+	{
+		// Desenha o número aleatório na tela
+		BeginDrawing();
+		ClearBackground(BLACK);
+
+		// Cria uma fonte raylib
+		Font fonte = GetFontDefault();
+
+		// Renderiza o número aleatório como um texto
+		char textoNumero[10];
+		sprintf(textoNumero, "%d", numeroAleatorio);
+		int larguraTexto = MeasureText(textoNumero, 36);
+		int xTexto = (screenWidth - larguraTexto) / 2;
+		int yTexto = screenHeight / 2;
+		DrawText(textoNumero, xTexto, yTexto, 36, WHITE);
+
+		// Atualiza a tela do jogo
+		EndDrawing();
+	}
+
+	// Sai do raylib
+	CloseWindow();
+
 	return 0;
 }
