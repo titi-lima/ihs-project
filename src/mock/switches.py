df5ef76b5ae30e886250640fa209dbf7bad8c624
Júlia Dias <julia.dias@citi.org.br>
2023-04-22 09:20:22 -0300

    refactor: store current switch array

--- src/mock/switches.py
+++ src/mock/switches.py
@@ -1,7 +1,12 @@
 class SwitchController:
+    switch_array = "000000001000000110"
+    
+    def __init__(self, switch_array):
+        self.switch_array = switch_array
+    
     # Get switch binary array
     def get_switch_array(self):
-        return "000000001000000110"
+        return self.switch_array
     
     def get_switch_number_1(self):
         switch = self.get_switch_array()[0:9]
@@ -13,4 +18,8 @@ class SwitchController:
 
     # Get individual switch binary value
     def get_switch_value(self, switch):
+        return self.switch_array[switch]
+    
+    def set_switch_array_value(self, value):
+        self.switch_array = value
         return 1
